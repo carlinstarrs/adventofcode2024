e34251923ba2f5a8480d6dd5305f96b180201b07
Carlin Starrs <carlinstarrs@gmail.com>
2024-11-30 21:28:59 -0800

    day 01

---
+++ day01.R
@@ -0,0 +1,14 @@
+read.csv(inputs / day01.txt)
+
+test <- function(this) {
+  this_is_a_function %>%
+    mutate(blah) %>%
+    pull(this)
+
+  return()
+}
+
+
+out %>%
+  test() %>%
+  this_is_fun()
