4c994b70c30739aee69208173c2cf630b6dacfd9
Carlin Starrs <carlinstarrs@gmail.com>
2024-11-30 22:00:42 -0800

    blargh

--- day01.R
+++ day01.R
@@ -1,14 +1,33 @@
-read.csv(inputs / day01.txt)
+library("tidyverse")
 
-test <- function(this) {
-  this_is_a_function %>%
-    mutate(blah) %>%
-    pull(this)
+#part 1
+input <- read.table("inputs/day01_input.txt")
+y1 <- setNames(input$V1, 1:length(input$V1))
+y2 <- setNames(input$V2, 1:length(input$V2))
 
-  return()
+dists <- c()
+
+for(i in 1:length(y1)){ 
+  # I should have just used sort here but I thought it was asking 
+  # the distance between the positions not the values
+  pos.min.y1 <- as.numeric(which.min(y1))
+  pos.min.y2 <- as.numeric(which.min(y2))
+
+  dists[i] <- abs(y1[pos.min.y1] - y2[pos.min.y2])
+  
+  y1[pos.min.y1] <- NA
+  y2[pos.min.y2] <- NA
 }
 
+sum(dists)
 
-out %>%
-  test() %>%
-  this_is_fun()
+#part 2
+similarities <- c()
+
+for(i in 1:length(y1)){
+  appearances <- length(y2[y2 == y1[i]])
+  
+  similarities[i] <- y1[i] * appearances
+}
+
+sum(similarities)
